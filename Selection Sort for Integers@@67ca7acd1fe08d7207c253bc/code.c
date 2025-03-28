a6433cc640f957c59ac4d64d6a9f9c44299b2ddb
Shipra-20 <arorashipra2005@gmail.com>
2025-03-28 20:04:06 +0530

    Solution to Selection Sort for Integers

--- Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
+++ Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 
-void bubbleSort(int arr[], int n) {
+void selectionsort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {  
